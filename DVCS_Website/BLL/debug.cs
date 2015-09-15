428b5a4064a662ddcd999b97a4076d4a6123f1cc
tjiang4 <tjiang1@ualberta.ca>
2015-09-15 16:17:10 -0600

    Add a BLL folder

---
+++ DVCS_Website/BLL/debug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for debug
+/// </summary>
+public class debug
+{
+	public debug()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
