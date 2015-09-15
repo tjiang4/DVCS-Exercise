1a78466349fc011909bda375ab523bb53dd12c94
tjiang4 <tjiang1@ualberta.ca>
2015-09-15 16:16:24 -0600

    Add a DAL folder

---
+++ DVCS_Website/DAL/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Context
+/// </summary>
+public class Context
+{
+	public Context()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
